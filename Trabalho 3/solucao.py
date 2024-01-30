c5aa7afdc948508d122277986d0ed97d3b7ba04f
Bruno Aires <brunoferreiraaires@gmail.com>
2024-01-29 22:59:15 -0300

    init astar hamming

--- Trabalho 3/solucao.py
+++ Trabalho 3/solucao.py
@@ -67,18 +67,43 @@ def expande(nodo:Nodo)->Set[Nodo]:
 
     return graph
 
+def reconstruir_caminho(node:Nodo):
+    caminho = []
+    while node:
+        caminho.append(node.acao)
+        node = node.pai
+    return caminho
+
+def hammingDistance(F:list[Nodo]) -> int:
+    finalState = '12345678_'
+    distance = None
+    i = 0
+    position = 0
+    for f in F:
+        actualDistance = sum(c1 != c2 for c1, c2 in zip(f.estado, finalState)) + f.custo
+        if (distance == None or actualDistance < distance):
+            distance = actualDistance
+            position = i
+        i += 1
+    return position
 
 def astar_hamming(estado:str)->list[str]:
-    """
-    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
-    retorna uma lista de ações que leva do
-    estado recebido até o objetivo ("12345678_").
-    Caso não haja solução a partir do estado recebido, retorna None
-    :param estado: str
-    :return:
-    """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    finalState = '12345678_'
+    X = [] ##Conjunto de Explorados
+    F: list[Nodo] = []
+    F.append(Nodo(estado, None, None, 1))
+    while F:
+        currentNode = F.pop(hammingDistance(F))
+        if(currentNode.estado == finalState):
+            return reconstruir_caminho(currentNode)
+        if(currentNode not in X):
+            X.append(currentNode)
+            a = expande(currentNode)
+            childrens = list(a)
+            for child in childrens:
+                if(child not in X):
+                    F.append(child)
+    return None
 
 
 def astar_manhattan(estado:str)->list[str]:
